d92fe216255fbcf9b99afb846a56baf2413a96ec
lixi <lxi1@binghamton.edu>
2024-03-17 23:25:22 -0400

    create pod and start figuring out how to scale pod

--- se_ml_production/experimental_containers/Subscribers/subscriber/subscriber.py
+++ se_ml_production/experimental_containers/Subscribers/subscriber/subscriber.py
@@ -10,10 +10,73 @@ from io import BytesIO
 from queue import Queue
 from google.cloud import storage
 from google.cloud import pubsub_v1
+from kubernetes import client, config
 
+# Create a pod
+def create_pod(job_id):
+    pod_name = f"pod-job-{job_id}"
+    container_name = f"container-{pod_name}"
+
+    # Define the container
+    container = client.V1Container(
+        name=container_name,
+        image="nginx:latest", 
+        ports=[client.V1ContainerPort(container_port=80)]
+    )
+
+    pod_spec = client.V1PodSpec(
+        containers=[container]  # Pass the container in a list
+    )
+
+    pod_metadata = client.V1ObjectMeta(name=pod_name)
+
+    pod = client.V1Pod(
+        api_version="v1",
+        kind="Pod",
+        metadata=pod_metadata,
+        spec=pod_spec
+    )
+
+    # Create an instance of the API class to interact with the K8s cluster
+    api_response = v1.create_namespaced_pod(
+        namespace="default",
+        body=pod
+    )
+    print("Pod created. Status='%s'" % str(api_response.status))
+
+def delete_pod(pod_name):
+    v1.delete_namespaced_pod(name=pod_name, namespace="default", body=client.V1DeleteOptions())
+    print(f"Pod {pod_name} deleted")
+
+# List all pods
+def list_all_pods():
+    # Configs can be set in Configuration class directly or using helper utility
+    config.load_kube_config("/Users/lixi/.kube/config")
+
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+# initiate k8s stuff
+config.load_kube_config("/Users/lixi/.kube/config")
+# instantiate a client for the V1 API group endpoint
+v1 = client.CoreV1Api()
 
 # Use a thread-safe queue instead of a list
 message_queue = Queue()
+
+def job_scheduler():
+    for job_id in message_queue:
+        create_pod(job_id)
+    
+    # put monitoring logic here
+    time.sleep(10)
+
+    for job_id in message_queue:
+        pod_name = f"pod-job-{job_id}"
+        delete_pod(pod_name)
 
 def process_data():
     def get_csv(db, target_csv_file): # Make this a private function
@@ -38,22 +101,33 @@ def process_data():
     while True:
         # Wait until a message is available
         message_data = message_queue.get()
-
+        print("message_data: ", message_data, "end")
         print(f"[INFO] Processing Data: {message_data}\n")
-        message_data = json.loads(message_data)
-        target_csv_file = message_data["upload_id"] + "-" + message_data["userID"] + ".csv"
 
-        client = storage.Client()
-        df = get_csv(client, target_csv_file)
+        name = 123
+        create_pod(name)
+        delete_pod(f"pod-job-{name}")
+        # run job scheduler
+        # job_scheduler()
 
-        print(df)
+        # test using postman - containerize the pod ... 
 
-        print("Doing some processing...")
-        time.sleep(10)  # Sleep 5 seconds to simulate processing
-        print("Processing Finished.")
 
-        # Signal that the processing is complete
-        message_queue.task_done()
+
+        # message_data = json.loads(message_data)
+        # target_csv_file = message_data["upload_id"] + "-" + message_data["userID"] + ".csv"
+
+        # client = storage.Client()
+        # df = get_csv(client, target_csv_file)
+
+        # print(df)
+
+        # print("Doing some processing...")
+        # time.sleep(10)  # Sleep 5 seconds to simulate processing
+        # print("Processing Finished.")
+
+        # # Signal that the processing is complete
+        # message_queue.task_done()
 
 def callback(message):
     message_queue.put(message.data.decode('utf-8'))
@@ -62,7 +136,7 @@ def callback(message):
     return
 
 def main():
-    subscription_id = "shiply_upload_csv-sub"
+    subscription_id = "test_topic-sub"
     project_id = "special-michelle"
 
     subscriber = pubsub_v1.SubscriberClient()
