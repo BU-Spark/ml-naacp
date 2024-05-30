67990e128f5bb0771b59e08c2da41a400b1e7387
Nikhil Ramchandani <ramchandaninikhil01@gmail.com>
2024-05-30 18:55:11 -0400

    spinner for log

--- se_ml_production/ML_backend_GKE/ML_Cloud_Run/Cloud_Run/bootstrappers.py
+++ se_ml_production/ML_backend_GKE/ML_Cloud_Run/Cloud_Run/bootstrappers.py
@@ -86,7 +86,9 @@ def bootstrap_MongoDB_Prod(client, defined_collection_names):
                 db_prod.create_collection(collection)
                 print(f"[INFO] Collection '{collection}' created.")
         spinner.stop()
-    	sys.stdout.write("Deployment Test Comlpete with no erors\n")
+		spinner = Spinner("Deployment Test Comlpete with no erors\n")
+		spinner.start()
+		spinner.stop()
     except Exception as err:
         spinner.err()
         print(f"[Error!] Error in Bootstrapping MongoDB Prod DB\nError: {err}")
